da24d8412743487b8e0a2e6eaa233d387a0083c7
Milad Vaghef <miladvaghefdev@gmail.com>
2025-03-07 14:57:32 +0330

    refactor: wrap "PathRoutes" component in main element
    Changed the HTML structure of the website, now the PathRoutes are inside the <main> element.

--- src/PathRoutes.tsx
+++ src/PathRoutes.tsx
@@ -6,12 +6,14 @@ import Contact from "./pages/Contact";
 
 export const PathRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/projects" element={<Projects />}></Route>
-      <Route path="/plans" element={<Plans />}></Route>
-      <Route path="/contact-me" element={<Contact />}></Route>
-      <Route path="*" element={<Home />}></Route>
-    </Routes>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/plans" element={<Plans />}></Route>
+        <Route path="/contact-me" element={<Contact />}></Route>
+        <Route path="*" element={<Home />}></Route>
+      </Routes>
+    </main>
   );
 };
