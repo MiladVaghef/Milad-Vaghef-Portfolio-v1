ea44a5db5cddc9710b7ed0a30747ab8ddc07d480
Milad Vaghef <miladvaghefdev@gmail.com>
2025-03-07 14:58:22 +0330

    doc: add comment to "PathRoutes"
    Added a few comments to "PathRoutes" for better readability.

--- src/PathRoutes.tsx
+++ src/PathRoutes.tsx
@@ -1,7 +1,12 @@
+// Import needed functions for router
 import { Routes, Route } from "react-router-dom";
+// Import home component
 import Home from "./pages/Home";
+// Import pages component
 import Projects from "./pages/Projects";
+// Import plans component
 import Plans from "./pages/Plans";
+// Import contact component
 import Contact from "./pages/Contact";
 
 export const PathRoutes = () => {
