eaf048f1d66a14587504db8c5cac0af09e73eccf
Milad Vaghef <143057362+MiladVaghef@users.noreply.github.com>
2025-03-26 05:14:16 +0330

    fix: remove unfinished files I accidently pushed
    I accidently pushed the files that were not finished and ready for pushing yet, so I removed them from repository.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,18 +1,5 @@
-import { lazy, Suspense } from "react";
-import { socialMediaData } from "../data/socialMedia";
-
-const SocialMedia = lazy(() => import("../components/SocialMedia"));
-
 const Contact = () => {
-  return (
-    <div id="contact">
-      <Suspense fallback={<div>Loading social media...</div>}>
-        {socialMediaData.map((mediaData, index) => (
-          <SocialMedia key={index} {...mediaData} />
-        ))}
-      </Suspense>
-    </div>
-  );
+  return <div>Contact</div>;
 };
 
 export default Contact;
