4a2d97b0d93ebcbc8820c17bc7b11ae8b63e9861
Milad Vaghef <143057362+MiladVaghef@users.noreply.github.com>
2025-11-14 03:56:54 +0330

    content: update projects and work history data
    I have updated the data in projects.ts and workHistory.ts data to 11/14/2025.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,5 @@
 import customadeLanding from "../assets/images/projects/customade-landing.png";
 import bushehrKhormaLanding from "../assets/images/projects/bushehr-khorma-landing.png";
-import miladVaghefLanding from "../assets/images/projects/milad-vaghef-landing.png";
 import cyberWorldLanding from "../assets/images/projects/cyber-world-landing.png";
 import netflixLoginPage from "../assets/images/projects/netflix-login-page.png";
 
@@ -27,17 +26,6 @@ export const projectsData: Project[] = [
     title: "Customade - Selling Custom Shoes",
     desc: "Commerce websites are my main source of works. Customade is a modern website for selling customized shoes.",
     tech: ["Wordpress", "Woocommerce", "Elementor"],
-  },
-  {
-    isPractice: false,
-    category: "Front-End",
-    activeLink: true,
-    link: "https://github.com/MiladVaghef/Milad-Vaghef-Portfolio-v1/",
-    image: miladVaghefLanding,
-    alt: "Milad Vaghef Landing",
-    title: "Milad Vaghef - Portfolio",
-    desc: "Well there’s not much to say, I like minimal designs for portfolio. I can design similar websites for your need.",
-    tech: ["JavaScript", "TypeScript", "React", "Scss"],
   },
   {
     isPractice: false,
@@ -82,16 +70,5 @@ export const projectsData: Project[] = [
     title: "Customade - Selling Custom Shoes",
     desc: "Commerce websites are my main source of works. Customade is a modern website for selling customized shoes.",
     tech: ["Figma", "Auto Layout"],
-  },
-  {
-    isPractice: false,
-    category: "Figma",
-    activeLink: false,
-    link: "#",
-    image: miladVaghefLanding,
-    alt: "Milad Vaghef Landing",
-    title: "Milad Vaghef - Portfolio",
-    desc: "Well there’s not much to say, I like minimal designs for portfolio. I can design similar websites for your need.",
-    tech: ["Figma", "Components", "Auto Layout", "Responsive"],
   },
 ];
--- src/data/workHistory.ts
+++ src/data/workHistory.ts
@@ -1,24 +1,38 @@
 export interface Project {
-    timeline: string;
-    title: string;
-    role: string[];
-    desc: string;
-    tech: string[];
-  }
-  
-  export const workHistoryData: Project[] = [
-    {
-      timeline: "2023 — Present",
-      title: "Freelancer",
-      role: ["Anything I can"],
-      desc: 'Freelancing is my main source of getting projects which I handle with lots of passions cause I deeply do care about people who trust on me with their websites. I deliver high quality websites with newest trends in web designing world. ',
-      tech: ["Everything"],
-    },
-    {
-      timeline: "2024 March — Present",
-      title: "Codiva Web Design Team",
-      role: ["Wordpress Developer","UI Designer"],
-      desc: 'Codiva is a web design team located in bushehr that focus on making useful websites for its clients which are mostly local business. I do help on anything that is needed in the team.',
-      tech: ["Wordpress","Figma","Javascript","HTML/CSS"],
-    },
-  ];
+  timeline: string;
+  title: string;
+  role: string[];
+  desc: string;
+  tech: string[];
+}
+
+export const workHistoryData: Project[] = [
+  {
+    timeline: "2023 — Present",
+    title: "Freelancer",
+    role: ["Anything I can"],
+    desc: "Freelancing is my main source of getting projects which I handle with lots of passions cause I deeply do care about people who trust on me with their websites. I deliver high quality websites with newest trends in web designing world. ",
+    tech: ["Everything"],
+  },
+  {
+    timeline: "2024 March — 2025 January ",
+    title: "Codiva Web Design Team",
+    role: ["Wordpress Developer", "UI Designer"],
+    desc: "Codiva is a web design team located in bushehr that focus on making useful websites for its clients which are mostly local business. I do help on anything that is needed in the team.",
+    tech: ["Wordpress", "Figma", "Javascript", "HTML/CSS", "Adobe"],
+  },
+  {
+    timeline: "2025 July — Present",
+    title: "Erfan Medical Supplier",
+    role: ["Digital Designer"],
+    desc: "At a local medical supplies store, I design digital assets and promotional banners for products and campaigns. I also develop UX concepts for their upcoming online shop, applying modern UI/UX principles to enhance their digital presence and improve customer experience.",
+    tech: ["Figma", "Adobe Photoshop", "Adobe Ilustrator"],
+  },
+  {
+    timeline: "2025 August — Present",
+    title: "Liankala - E-commerce Website Specialist",
+    role: ["Digital Designer"],
+    desc: "I support the Liankala platform by managing and optimizing its content and adding new products while making them SEO-friendly. My work keeps the website organized, efficient, and user-friendly, ensuring smooth operation and improved performance across a wide range of products.",
+    tech: ["E-commerce CMS", "Content Management", "SEO Optimization"],
+  },
+];
